e3afe9cb463110830041392d07f3be8bef37b8f1
kimilguk <kimilguk19@gmail.com>
2025-07-27 15:06:30 +0900

    2주차 1교시 작업소스3

--- bin/dart_console.dart
+++ bin/dart_console.dart
@@ -1,5 +1,14 @@
-import 'package:dart_console/dart_console.dart' as dart_console;
+/*import 'package:dart_console/dart_console.dart' as dart_console;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_console.calculate()}!');
+}*/
+/**
+ * 변수와 함수 및 클래스를 사용하는 앱
+ * 제작자 : 학생명
+ */
+void main() { // 콘솔 앱 실행 진입점
+  int myAge; // 변수명은 카멜표기법(소문자로시작, 2번째 단어 시작은 대문자로)
+  myAge = 30; // 변수 모양이 낙타등 처럼 보여서 낙타등 표기법 이라고도 한다.
+  print(myAge); // print는 Dart에 내장된 출력 함수 이다.
 }
